aaea4d8fc703bce6acd8d4b86721ba043c2fa12b
AdrianLoof <adrian.loof.0474@student.uu.se>
2021-11-16 13:48:34 +0100

    Börjat med vue och js

--- src/components/Burger.vue
+++ src/components/Burger.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
-  </div>
+
+
+      <div>
+        <figure>
+          <h3 v-bind:key=burger.name> {{ burger.name }}</h3>
+          <img v-bind:src="burger.img"  style="width: 200px;">
+          <ul>
+            <li v-bind:key=burger.kCal> <span class="ingrediens">{{ burger.kCal }}</span> Calories</li>
+            <li v-if="burger.lactose">Contains <span class="ingrediens">Lactose</span></li>
+            <li v-else>Lactose <span class="ingrediens">Free</span></li>
+            <li v-if="burger.gluten"> Contains <span class="ingrediens">Gluten</span></li>
+            <li v-else> Gluten <span class="ingrediens">Free</span></li>
+          </ul>
+        </figure>
+      </div>
+
+
+
+
 </template>
 
 <script>
@@ -15,5 +31,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#testid {
+  text-transform: uppercase;
+  background-color: black;
+  color: white;
+  border: dotted;
 
+}
+
+.wrapper {
+  display: grid;
+  grid-gap: 10px;
+  padding: 10px;
+  background-color: #726d6d;
+  color: #111010;
+}
+
+
+.a {
+  grid-column: 1;
+  border: groove #ff5500;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,96 @@
 <template>
+  <header>
+    <div class="container">
+      <h1 id="headertext">Välkommen till Addes Burgare</h1>
+      <img id="background" src="paddys.jpg">
+    </div>
+  </header>
+
+  <main>
+    <section id="testid">
+      <h2>Välj Burgare</h2>
+      <p>This is where you select burger</p>
+      <div class="wrapper">
+        <Burger class="box" v-for="burger in burgers"
+                v-bind:burger="burger"
+                v-bind:key="burger.name"/>
+      </div>
+    </section>
+
+    <section id="testid2">
+      <h3 class="movetitles">Customer Information</h3>
+      <form>
+        <p>
+          <input type="radio" id="male" name="gender" value="male">
+          <label for="male">Male</label>
+        </p>
+
+        <p>
+          <input type="radio" id="female" name="gender" value="female">
+          <label for="female">Female</label>
+        </p>
+
+        <p>
+          <input type="radio" id="Other" name="gender" value="Other" checked>
+          <label for="Other">Other</label>
+        </p>
+
+
+        <h4 class="movetitles">Delivery Information</h4>
+
+
+        <p>
+          <label for="Fullname">Full Name</label><br>
+          <input type="text" id="fullname" name="fn" required="required" placeholder="Full Name">
+        </p>
+
+        <p>
+          <label for="email">Email</label><br>
+          <input type="email" id="email" name="em" required="required" placeholder="E-mail address">
+        </p>
+
+        <p>
+          <label for="Street">Street</label><br>
+          <input type="text" id="street" name="st" placeholder="Street">
+        </p>
+
+        <p>
+          <label for="HouseNumber">House Number</label><br>
+          <input type="number" id="housenumber" name="Num" placeholder="House Number">
+        </p>
+
+        <p>
+          <label for="recipient">Recipient</label><br>
+          <select id="recipient" name="rcp">
+            <option selected="selected">Credit Card</option>
+            <option>Delivery Payment</option>
+            <option>Bitcoin</option>
+            <option>Invoice</option>
+            <option>Sing a song</option>
+          </select>
+        </p>
+        <p>
+          <label for="other">Other Information</label><br>
+          <textarea id="other"></textarea>
+        </p>
+      </form>
+
+    </section>
+    <button class="button" style="margin-left: 20px  " type="submit">
+
+      Make Order!
+    </button>
+
+  </main>
+  <footer>
+    <hr>
+    Please do not sue me
+  </footer>
+  <input type="text" v-model="yourVariable">
   <div>
-    Burgers
-    <Burger v-for="burger in burgers"
-            v-bind:burger="burger" 
-            v-bind:key="burger.name"/>
+    {{ yourVariable }}
   </div>
+
   <div id="map" v-on:click="addOrder">
     click here
   </div>
@@ -16,6 +102,22 @@ import io from 'socket.io-client'
 
 const socket = io();
 
+function MenuItem(Name, calories, picture, gluten,milk) {
+  this.name = Name; // The *this* keyword refers to the object itself
+  this.ingredient =gluten ;
+  this.lactose=milk;
+  this.img = picture;
+  this.kCal=calories;
+  return this;
+}
+
+const BurgerArray=[
+  new MenuItem("Addes",1200,"https://images.unsplash.com/photo-1613277367862-f8ef14db7748?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmlnJTIwYnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",false,true),
+  new MenuItem( "Robbans", 400,"https://www.schaer.com/sites/default/files/2000_Fiery%20Mexican%20Chicken%20Burger.jpg", true,  true ),
+  new MenuItem("Elibonks", 300,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP6JTkxNoHt9VElHKqalvT_8tVus0wQE61JA&usqp=CAU", false,  false)
+];
+console.log(BurgerArray);
+
 export default {
   name: 'Home',
   components: {
@@ -23,10 +125,13 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
+       yourVariable: "välj en burgare",
+      burgers: BurgerArray
+
+          /*[ {name: "small burger", kCal: 250},
                  {name: "standard burger", kCal: 450},
                  {name: "large burger", kCal: 850}
-               ]
+               ] */
     }
   },
   methods: {
@@ -48,7 +153,115 @@ export default {
 </script>
 
 <style>
-  #map {
+@import 'https://fonts.googleapis.com/css?family=Pacifico|Dosis';
+
+body {
+  font-family: Arial, sans-serif;
+  font-size: 15pt;
+}
+
+section {
+  margin: 20px;
+}
+
+section div {
+  padding: 60px;
+}
+
+.ingrediens {
+  color: #ff5500;
+}
+
+.movetitles {
+  margin-left: 10px;
+}
+
+#testid {
+  text-transform: uppercase;
+  background-color: black;
+  color: white;
+  border: dotted;
+
+}
+
+#testid2 {
+  background-color: white;
+  color: black;
+  border: dotted #111010;
+
+
+}
+
+form p {
+  padding-left: 30px;
+}
+
+.button:hover {
+  cursor: pointer;
+  background-color: chartreuse;
+}
+
+.container{
+  position:relative;
+  text-align:center;
+  color:white;
+  margin:20px;
+  border:groove #ff5500;
+  overflow:hidden;
+}
+#background{
+  align-content: center;
+  width: 100%;
+  height: 13em;
+  opacity:70%;
+}
+
+#headertext{
+  position :absolute;
+  top :30%;
+  left: 50%;
+  z-index:1;
+  transform: translate(-50%,50%);
+}
+
+.wrapper {
+  /*display: grid;
+  grid-gap: 40px;
+ padding:100px; */
+
+  background-color: #726d6d;
+  color: #111010;
+  display: grid;
+  grid-gap: 250px;
+  grid-template-columns: 100px 100px 100px;
+
+
+}
+.box {
+ /* background-color: #444;
+  color: #fff;
+  border-radius: 50px;
+  padding: 50px;
+   font-size: 150%; */
+}
+
+
+.a {
+  grid-column: 1;
+
+}
+.b {
+  grid-column: 2 ;
+
+}
+.c {
+  grid-column: 3 ;
+
+}
+
+
+
+#map {
     width: 300px;
     height: 300px;
     background-color: red;
